c329461bb2488be00c429a5fc7e33f80fdc83f08
Bondo <m@bon.do>
2018-08-28 22:26:36 +0200

    fix

--- Actions/AddOnNewBranchTranslation.cs
+++ Actions/AddOnNewBranchTranslation.cs
@@ -56,6 +56,7 @@ namespace translate_spa.Actions
             foreach (var item in translations)
             {
                 item.SetNewId();
+                item.Branch = branch;
             }
 
             SetEnvionment(translations);
